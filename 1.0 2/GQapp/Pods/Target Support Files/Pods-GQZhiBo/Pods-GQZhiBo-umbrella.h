3492c44186ecad4d17ad7ad7e37c24ad8cb1ac14
geng199200 <1247759034@qq.com>
2018-09-03 13:46:40 +0800

    fix target Name

--- 1.0 2/GQapp/Pods/Target Support Files/Pods-GQZhiBo/Pods-GQZhiBo-umbrella.h
+++ 1.0 2/GQapp/Pods/Target Support Files/Pods-GQZhiBo/Pods-GQZhiBo-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_newGQappVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_newGQappVersionString[];
+FOUNDATION_EXPORT double Pods_GQZhiBoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GQZhiBoVersionString[];
 
--- 1.0 2/GQapp/Pods/Target Support Files/Pods-newGQapp/Pods-newGQapp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_newGQapp : NSObject
-@end
-@implementation PodsDummy_Pods_newGQapp
-@end
